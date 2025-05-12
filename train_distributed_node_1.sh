11816b7c56793bb5261acbdac6d374f03f193037
zixiao <shunli.dsl@alibaba-inc.com>
2025-05-12 18:38:57 +0800

    update.

---
+++ train_distributed_node_1.sh
@@ -0,0 +1,8 @@
+export ATTN_BACKEND=xformers
+export LD_LIBRARY_PATH=/usr/local/cuda-12.4/lib64/:$LD_LIBRARY_PATH
+MASTER_ADDR=192.168.2.99
+MASTER_PORT=12233
+RANK=1
+NODES=2
+rm -rf outputs/*
+python train.py --config configs/vae/slat_vae_dec_mesh_swin8_B_64l8_fp16.json --output_dir outputs/slat_vae_dec_mesh_swin8_B_64l8_fp16_1node --data_dir datasets/ObjaverseXL_sketchfab/   --num_nodes $NODES --node_rank $RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT
